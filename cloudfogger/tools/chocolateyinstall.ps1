a8821f9723c64568bb9b383c85c875f2f4560211
Sven Röttjer <sven.roettjer@web.de>
2016-02-21 14:55:57 +0100

    first version of cloudfogger package

---
+++ cloudfogger/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'cloudfogger'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.cloudfogger.com/download/Cloudfogger_Setup.exe'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName  = 'cloudfogger*'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cloudfogger/tools/chocolateyuninstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'cloudfogger'
+$installerType = 'EXE'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+$registryPath32 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{E5A0D9A8-3711-4DB2-833C-FF79EF65830D}_is1'
+$registryPathWow6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{E5A0D9A8-3711-4DB2-833C-FF79EF65830D}_is1'
+
+if (Test-Path $registryPath32) {
+$registryPath = $registryPath32
+}
+
+if (Test-Path $registryPathWow6432) {
+$registryPath = $registryPathWow6432
+}
+
+if ($registryPath) {
+$uninstallString = (Get-ItemProperty -Path $registryPath -Name 'UninstallString').UninstallString
+}
+
+if ($uninstallString) {
+stop-process -Name cloudfogger -force
+Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $uninstallString
+}
