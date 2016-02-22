c2d4c0b84bc273c0f9542a5707d4864d3510408b
Sven Röttjer <sven.roettjer@web.de>
2016-02-22 19:40:51 +0100

    1. Removed $url64 2. Fixed url to package source (packageSourceUrl)

--- cloudfogger/tools/chocolateyinstall.ps1
+++ cloudfogger/tools/chocolateyinstall.ps1
@@ -1,18 +1,14 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-
-
 $packageName= 'cloudfogger'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://www.cloudfogger.com/download/Cloudfogger_Setup.exe'
-$url64      = $url
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName  = 'cloudfogger*'
 }
